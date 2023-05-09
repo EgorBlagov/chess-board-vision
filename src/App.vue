bd7171708dd1d5f9a6b4c4e5978955d5291e2593
Благов Егор <e.blagov@tinkoff.ru>
2023-05-09 18:01:54 +0400

    Chess board practice

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import ChessBoard from './components/ChessBoard.vue';
+import InfoBar from './components/InfoBar.vue';
+import { BOARD_SIZE } from './stores/stores';
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="child-container">
+        <InfoBar />
+        <div class="board-square">
+          <ChessBoard :size="BOARD_SIZE" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.child-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+.board-square {
+  aspect-ratio: 1/1;
+  height: 80vmin;
+}
+</style>
---
+++ src/components/InfoBar.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { usePause, useTimer, useTargetStore } from '@/stores/stores';
+
+const pauseStore = usePause();
+const timerStore = useTimer();
+const targetStore = useTargetStore();
+</script>
+
+<template>
+  <div class="body">
+    <div class="header">Chess board Vision</div>
+    <div class="controls">
+      <div
+        class="pause-bar"
+        @mouseenter="pauseStore.onHover(true)"
+        @mouseleave="pauseStore.onHover(false)"
+      >
+        ⏸
+      </div>
+      <div
+        class="current-color"
+        :class="{ white: targetStore.isWhite }"
+        @click="targetStore.toggle()"
+      >
+        {{ targetStore.isWhite ? 'white' : 'black' }}
+      </div>
+      <div class="target-code">
+        <div :class="{ white: targetStore.isWhite }">{{ targetStore.code }}</div>
+      </div>
+      <div class="filler"></div>
+      <div class="time">{{ timerStore.timePassed.toFixed(2) }}s</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  user-select: None;
+  width: 100%;
+}
+
+.controls {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5em;
+  font-size: 2em;
+  width: 100%;
+}
+
+.header {
+  font-size: 1.5em;
+  font-weight: 900;
+}
+
+.filler {
+  flex: 1 1 auto;
+}
+.pause-bar {
+  opacity: 0.5;
+  background-color: black;
+  color: white;
+  padding-left: 0.3em;
+  padding-right: 0.3em;
+  border-radius: 0.2em;
+}
+
+.current-color {
+  font-weight: 900;
+  cursor: pointer;
+}
+
+.target-code {
+  /* padding: 0.2em; */
+  font-weight: 900;
+}
+.time {
+  font-family: 'Lucida Console', 'Courier New', monospace;
+  font-weight: 900;
+}
+.white {
+  color: white;
+  text-shadow: 0 0 2px #000, 0 0 2px #000, 0 0 2px #000;
+}
+</style>
